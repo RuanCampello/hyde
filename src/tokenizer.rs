9842501db5342087d17d03eb1845d9133c7284c7
RuanCampello <ruancampello888@gmail.com>
2024-12-08 08:32:30 -0300

    feat: add basic operations (NOT, AND, OR)

---
+++ src/tokenizer.rs
@@ -0,0 +1,144 @@
+use std::collections::HashMap;
+
+#[derive(PartialEq)]
+enum Token {
+    Exp(char),
+    Conjunction,
+    Disjunction,
+    Conditional,
+    Biconditional,
+    Negation,
+    LeftParen,
+    RightParen,
+}
+
+/// Interface responsible to tokenize and operate over a given expression.
+pub struct Tokenizer<'a> {
+    expression: &'a str,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(expression: &'a str) -> Self {
+        Self { expression }
+    }
+
+    // TODO make prepositions optional
+    pub fn parse_and_evaluate(&self, prepositions: &HashMap<char, bool>) -> Result<bool, String> {
+        let tokens: Vec<Token> = self.tokenize();
+
+        let mut operators: Vec<&Token> = Vec::new();
+        let mut output: Vec<bool> = Vec::new();
+
+        tokens.iter().try_for_each(|token| match token {
+            Token::Exp(c) => {
+                if let Some(&value) = prepositions.get(c) {
+                    output.push(value);
+                    Ok(())
+                } else {
+                    Err(format!("Variable {} is undefined", c))
+                }
+            }
+            Token::LeftParen => {
+                operators.push(&Token::LeftParen);
+                Ok(())
+            }
+            Token::RightParen => {
+                while let Some(top_op) = operators.pop() {
+                    if top_op == &Token::LeftParen {
+                        break;
+                    }
+                    Self::apply_operator(top_op, &mut output)?;
+                }
+                Ok(())
+            }
+            _ => {
+                while operators.last().map_or(false, |&top_op| {
+                    Self::precedence(top_op) >= Self::precedence(token)
+                        && *top_op != Token::LeftParen
+                }) {
+                    let op = operators.pop().unwrap();
+                    Self::apply_operator(op, &mut output)?;
+                }
+                operators.push(token);
+                Ok(())
+            }
+        })?;
+
+        while let Some(op) = operators.pop() {
+            Self::apply_operator(op, &mut output)?;
+        }
+
+        output.pop().ok_or(String::from("Invalid expression"))
+    }
+
+    fn tokenize(&self) -> Vec<Token> {
+        self.expression
+            .chars()
+            .filter_map(|c| match c {
+                '∧' => Some(Token::Conjunction),
+                '∨' => Some(Token::Disjunction),
+                '¬' => Some(Token::Negation),
+                '→' => Some(Token::Conditional),
+                '↔' => Some(Token::Biconditional),
+                '(' => Some(Token::LeftParen),
+                ')' => Some(Token::RightParen),
+                c if c.is_alphabetic() => Some(Token::Exp(c)),
+                _ => None,
+            })
+            .collect()
+    }
+
+    /// Determines the precedence order of each operation/token.
+    fn precedence(operation: &Token) -> usize {
+        match operation {
+            Token::Negation => 1,
+            Token::Conjunction => 2,
+            Token::Disjunction => 3,
+            Token::Conditional => 4,
+            Token::Biconditional => 5,
+            _ => 0, // in case of parenthesis
+        }
+    }
+
+    /// Applies the operation on the operands and remove them from the to-do operations stack.
+    fn apply_operator(op: &Token, stack: &mut Vec<bool>) -> Result<(), String> {
+        match op {
+            Token::Negation => {
+                let operand = stack.pop().ok_or("Missing operand for NOT")?;
+                stack.push(!operand);
+            }
+            Token::Conjunction => {
+                let right = stack.pop().ok_or("Missing right operand for AND")?;
+                let left = stack.pop().ok_or("Missing left operand for AND")?;
+                stack.push(left && right);
+            }
+            Token::Disjunction => {
+                let right = stack.pop().ok_or("Missing right operand for OR")?;
+                let left = stack.pop().ok_or("Missing left operand for OR")?;
+                stack.push(left || right);
+            }
+            // TODO other operations...
+            _ => return Err("Unexpected operator".into()),
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_operations() {
+        let exp = "p∨q";
+        let tokenizer = Tokenizer::new(exp);
+
+        let mut prep: HashMap<char, bool> = HashMap::new();
+        prep.insert('p', true);
+        prep.insert('q', false);
+
+        let result = tokenizer.parse_and_evaluate(&prep);
+
+        assert!(result.unwrap());
+    }
+}
